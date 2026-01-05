23f4143a8cdc916beeb0bed453af61896010df83
Litudalei <gitiranjan815@gmail.com>
2026-01-05 10:36:45 +0530

    case 2 oops banner app

---
+++ USECASE2/OOPSBanner.java
@@ -0,0 +1,53 @@
+package USECASE2;
+
+public class OOPSBanner {
+
+public static void main(String[] args) {
+uc1();
+        uc2();
+}
+
+public static void uc1() {
+String str = "OOPS";
+System.out.println(str);
+}
+
+    public static void uc2() {
+        String[] O = {
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+        String[] P = {
+                "*******  ",
+                "*      * ",
+                "*      * ",
+                "*******  ",
+                "*        ",
+                "*        ",
+                "*        "
+        };
+        String[] S = {
+                " ******* ",
+                " *       ",
+                " *       ",
+                " ******* ",
+                "       * ",
+                "       * ",
+                " ******* "
+        };
+        String[][] oops = {O, O, P, S};
+        for (int row = 0; row < O.length; row++) {
+            StringBuilder line = new StringBuilder();
+            for (String[] letter : oops) {
+                line.append(letter[row]).append(" ");
+            }
+            System.out.println(line.toString());
+        }
+    }
+}
+
